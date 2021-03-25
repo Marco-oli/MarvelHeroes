ab116113ded80ab8d5cf7e06625c81f4a456b1c0
Marco-oli <marcoaurelio.oliveira@sambatech.com.br>
2021-03-25 16:59:07 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle={'light-content'} />
+    </View>
+  );
+};
+
+export default App;
