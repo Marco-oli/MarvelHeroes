9c70ec7a71f4055233fec83e8471c535789e187e
Marco-oli <marcoaurelio.oliveira@sambatech.com.br>
2021-03-26 09:32:39 -0300

    adicionando novas imagens

--- src/components/CardHeroes/index.js
+++ src/components/CardHeroes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, ImageBackground, StyleSheet} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 
 import * as S from './styles';
 import {CardSubtitle, CardTitle} from '../../assets/typography';
@@ -7,8 +7,7 @@ import {CardSubtitle, CardTitle} from '../../assets/typography';
 const CardHeroes = ({name, image, heroName}) => {
   const styles = StyleSheet.create({
     styleImage: {
-      width: 140,
-      height: 230,
+      flex: 1,
       padding: 12,
       justifyContent: 'flex-end',
     },
@@ -16,11 +15,8 @@ const CardHeroes = ({name, image, heroName}) => {
 
   return (
     <S.Container>
-      <ImageBackground
-        source={require('../../assets/chars/spider-man.png')}
-        style={styles.styleImage}>
+      <ImageBackground source={{uri: image}} style={styles.styleImage}>
         <S.ContainerText>
-          {console.log(image)}
           <CardSubtitle>{name}</CardSubtitle>
           <CardTitle>{heroName}</CardTitle>
         </S.ContainerText>
